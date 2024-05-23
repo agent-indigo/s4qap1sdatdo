8c246f2766aaecbe04781c439d169afbd28718bb
agent-indigo <bradenhynes@gmail.com>
2024-05-23 08:58:30 -0230

    Code formatting & removed empty class

--- src/main/java/com/keyin/SuggestionsDatabase.java
+++ src/main/java/com/keyin/SuggestionsDatabase.java
@@ -1,19 +1,12 @@
 package com.keyin;
-
 import java.util.HashMap;
 import java.util.Map;
-
 public class SuggestionsDatabase {
     private Map<String,Integer> wordMap;
-
     public Map<String, Integer> getWordMap() {
-        if (wordMap == null) {
-            wordMap = new HashMap<String, Integer>();
-        }
-
+        if (wordMap == null) wordMap = new HashMap<String, Integer>();
         return wordMap;
     }
-
     public void setWordMap(Map<String, Integer> wordMap) {
         this.wordMap = wordMap;
     }
